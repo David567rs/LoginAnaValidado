b566b1ceab6d4c6354df085e2be272c5f30771d2
David567rs <resendez515@gmail.com>
2025-12-03 00:49:59 -0600

    Url

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,19 +6,21 @@ import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const config = app.get(ConfigService)
-  const frontendUrl = config.get<string>('https://inhalex.netlify.app') || 'http://localhost:3000'
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
 
-  app.use(helmet())
-  app.enableCors({ origin: frontendUrl, credentials: true })
-  app.setGlobalPrefix('api')
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+  const frontendUrl =
+    config.get<string>('FRONTEND_URL') || 'http://localhost:3000';
 
-  const port = process.env.PORT ? Number(process.env.PORT) : 4000
-  await app.listen(port)
-  // eslint-disable-next-line no-console
-  console.log(`API listening on http://localhost:${port}`)
+  app.use(helmet());
+  app.enableCors({ origin: frontendUrl, credentials: true });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+  await app.listen(port);
+  console.log(`API listening on http://localhost:${port}`);
 }
 
-bootstrap()
+bootstrap();
+
